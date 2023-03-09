5b6a0e18dd2867cb6ac3e2efb9b01597168014e3
isac <116252006+isacdevaabishek@users.noreply.github.com>
2023-03-09 18:22:42 +0530

    isac

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -18,12 +18,31 @@ function generate() {
     let output=" ";
    
     let capital = document.getElementById("uppercase").checked
+    let small=document.getElementById("lowercase").checked
     let arr =   [...Array(26)].map((_, i) => String.fromCharCode(i + (capital ? 65 : 97)))
+    let arr1=[...Array(26)].map((_, i) => String.fromCharCode(i + 97))
+    
     console.log(arr);
     for(i=1;i<=isac;i++){
-    if (capital == true) {
+    if (capital == true&&small==false) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         const item = arr[randomIndex];
+     password=item
+     output=output+password
+     document.getElementById('password__result').value = output;
+    }  
+    if (small == true&&capital==false) {
+        const randomIndex = Math.floor(Math.random() * arr1.length);
+        const item = arr1[randomIndex];
+     password=item
+     output=output+password
+     document.getElementById('password__result').value = output;
+    }  
+    if (small == true&&capital == true) {
+        let smcap=arr.concat(arr1)
+        console.log(smcap);
+        const randomIndex = Math.floor(Math.random() * smcap.length);
+        const item = smcap[randomIndex];
      password=item
      output=output+password
      document.getElementById('password__result').value = output;
