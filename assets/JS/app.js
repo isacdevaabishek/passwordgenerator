2a2d77ca582f644c18a1af94e5f5b0d16f01f0c9
isac <116252006+isacdevaabishek@users.noreply.github.com>
2023-03-09 18:30:25 +0530

    number

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -19,26 +19,57 @@ function generate() {
    
     let capital = document.getElementById("uppercase").checked
     let small=document.getElementById("lowercase").checked
+    let numbers=document.getElementById("numbers").checked
     let arr =   [...Array(26)].map((_, i) => String.fromCharCode(i + (capital ? 65 : 97)))
     let arr1=[...Array(26)].map((_, i) => String.fromCharCode(i + 97))
-    
+    let arr2=["1","2","3","4","5","6","7","8","9","0"]
     console.log(arr);
     for(i=1;i<=isac;i++){
-    if (capital == true&&small==false) {
+        // for only capital 
+    if (capital == true&&small==false&&number==false) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         const item = arr[randomIndex];
      password=item
      output=output+password
      document.getElementById('password__result').value = output;
     }  
-    if (small == true&&capital==false) {
+
+    // for only small 
+    if (small == true&&capital==false&&number==false) {
         const randomIndex = Math.floor(Math.random() * arr1.length);
         const item = arr1[randomIndex];
      password=item
      output=output+password
      document.getElementById('password__result').value = output;
     }  
-    if (small == true&&capital == true) {
+
+    // for number only 
+    if (numbers == true&&capital==false&&small==false) {
+        const randomIndex = Math.floor(Math.random() * arr2.length);
+        const item = arr2[randomIndex];
+     password=item
+     output=output+password
+     document.getElementById('password__result').value = output;
+    }  
+//   for number and capital 
+    if (numbers == true&&capital == true&&small==false) {
+        const randomIndex = Math.floor(Math.random() * arr2.length);
+        const item = arr2[randomIndex];
+     password=item
+     output=output+password
+     document.getElementById('password__result').value = output;
+    }  
+// for number and small 
+    if (numbers == true&&small == true&&capital==false) {
+        const randomIndex = Math.floor(Math.random() * arr2.length);
+        const item = arr2[randomIndex];
+     password=item
+     output=output+password
+     document.getElementById('password__result').value = output;
+    }  
+// for small and cap 
+
+    if (small == true&&capital == true&&number==false) {
         let smcap=arr.concat(arr1)
         console.log(smcap);
         const randomIndex = Math.floor(Math.random() * smcap.length);
